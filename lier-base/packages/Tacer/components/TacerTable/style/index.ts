60411bdf13fa2fd044ce2d61b861f01834a5ddca
胡标 <biao.hu@baishan.com>
2023-05-10 13:05:40 +0800

    Bump version 0.1.7

--- lier-base/packages/Tacer/components/TacerTable/style/index.ts
+++ lier-base/packages/Tacer/components/TacerTable/style/index.ts
@@ -1,7 +1,9 @@
 // 用于样式的按需加载
 // 如此组件依赖了 B 组件，则需要将 B 组件的样式也引用进来，如: import '../../ComponentB/style'
-import './index.less';
 import '@arco-design/web-react/es/Button/style';
 import '@arco-design/web-react/es/Popconfirm/style';
 import '@arco-design/web-react/es/Space/style';
 import '@arco-design/web-react/es/Table/style';
+import '../../TacerModal/style';
+import '../../TacerSearch/style';
+import './index.less';
