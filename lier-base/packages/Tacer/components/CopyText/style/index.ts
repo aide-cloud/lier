e2c534568e321485688cdbb998d6110584165366
梧桐 <aidecloud@163.com>
2023-06-10 16:48:34 +0800

    Bump version 0.1.49

--- lier-base/packages/Tacer/components/CopyText/style/index.ts
+++ lier-base/packages/Tacer/components/CopyText/style/index.ts
@@ -2,4 +2,6 @@
 // 如此组件依赖了 B 组件，则需要将 B 组件的样式也引用进来，如: import '../../ComponentB/style'
 import '@arco-design/web-react/es/Message/style';
 import '@arco-design/web-react/es/Tooltip/style';
+import '@arco-design/web-react/es/Button/style';
+import '@arco-design/web-react/es/Tag/style';
 import './index.less';
